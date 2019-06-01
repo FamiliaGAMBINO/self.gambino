f689e88d72ab8b57bda01c0db9be4fb4cf4fcf3c
FamiliaGAMBINO <51273109+FamiliaGAMBINO@users.noreply.github.com>
2019-06-01 21:34:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ const client = new Discord.Client();
 client.login (process.env.TOKEN);
 
 const activities_list = [
-  "𝓦.𝓓.𝓢",
-  "𝑨 𝑫𝑰𝑺𝑪𝑶𝑹𝑫 𝑻𝑬𝑨𝑴",
-  "𝑶𝑭 𝑹𝑨𝑳𝑶𝑼𝑭𝑺",
+  "son cercueil.",
     ]; // creates an arraylist containing phrases you want your bot to switch through.
 
 client.on('ready', () => {
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
         client.user.setActivity(activities_list[index],{
-            type: "LOOKING",
-            url: "https://www.twitch.tv/evil"});
+            type: "WATCHING",
+            url: "https://www.twitch.tv/alcapone"});
  
     }, 3000); // Runs this every 10 seconds. 1000 = 1seconde
     console.log('Connecté');
